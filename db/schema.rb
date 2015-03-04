3c409500ecf325443866d970076c5c23dd922d94
Colin Rubbert <colinrubbert@gmail.com>
2015-03-04 02:12:57 +0000

    Adjust styling and add Account User Name to db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150301213926) do
+ActiveRecord::Schema.define(version: 20150304005815) do
 
   create_table "todo_items", force: true do |t|
     t.string   "content"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20150301213926) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
